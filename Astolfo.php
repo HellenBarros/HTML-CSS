ce006a5551391c0b2f28746a1ba192193a6bb06c
HellenBarros <hellenbarrosac@gmail.com>
2022-06-06 12:11:03 -0300

    Tudo já feito

---
+++ Astolfo.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset = "uft-8">
+	<title> Astolfo </title>
+	<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<header>
+			<img src="Astolfo.jpeg" alt = "imagem de astolfo" class="first_image">
+			<h1 id = "title"> Astolfo Raj Peti</h1>
+		</header>
+		<section>
+			<header>
+				<h2 class="subtitle">Post</h2>
+			</header>
+			<article class = "post">
+				<header>
+					<h3 class="post_title">Post #1</h3>
+					<img src="astolfoday1.jpeg" alt="ilustração do primeiro dia de astolfo" class="post_image">
+				</header>
+				<p class= "post_content">
+				Olá! Eu sou Astolfo, e fui adotado no dia 18/05/2022 por <a href="https://instagram.com/ibarros.hellen?igshid=YmMyMTA2M2Y="target="_blank">Hellen</a>. Sou um porquinho-da-índia de 3 cores, marrom, preto e branco minha comida favorita é Cenoura e ração, ainda estou me desafiando a comer outras coisas. Sou filho único mas recebo toda atenção que preciso. Amo correr pelo quarto e estou sempre atento a tudo. Aqui será relatado minhas aventuras. Bjinhos.
+				</p>
+			</article>
+		</section>
+		<footer>
+			<ul class="contact_list">
+				<li>
+					<a href="mailto:hellenbarrosac@gmail.com">hellenbarrosac@gmail.com</a>
+				</li>
+				<li>
+					<a href="https://instagram.com/ibarros.hellen?igshid=YmMyMTA2M2Y="target="_blank">Instagram</a>
+				</li>
+			</ul>
+		</footer>
+	</body>
+	
+</html>
+
